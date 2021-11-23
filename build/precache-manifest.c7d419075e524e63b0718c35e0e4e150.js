81ecb27c02266ae7db9199e207c9dbf44d6386c8
Sathya Boopathy <Sathya.p@kovantech.com>
2021-11-23 12:10:44 -0600

    initial commit

---
+++ build/precache-manifest.c7d419075e524e63b0718c35e0e4e150.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9235c7a9c2f5b3ddcfe",
+    "url": "/bake/static/css/main.21eacc69.chunk.css"
+  },
+  {
+    "revision": "b9235c7a9c2f5b3ddcfe",
+    "url": "/bake/static/js/main.ca183c7d.chunk.js"
+  },
+  {
+    "revision": "15c56431bad20e882ef5",
+    "url": "/bake/static/js/runtime~main.086dfcfd.js"
+  },
+  {
+    "revision": "bc0f08ec7ec538e9d7b1",
+    "url": "/bake/static/css/2.c3c04259.chunk.css"
+  },
+  {
+    "revision": "bc0f08ec7ec538e9d7b1",
+    "url": "/bake/static/js/2.28c1f0d6.chunk.js"
+  },
+  {
+    "revision": "c1978c173fc3f97f6a7ba6424d3f9a70",
+    "url": "/bake/static/media/contact-image.c1978c17.jpg"
+  },
+  {
+    "revision": "eccb0c02f94ed4264a8415ce439d30f6",
+    "url": "/bake/static/media/bake-baked-bakery-1028714.eccb0c02.jpg"
+  },
+  {
+    "revision": "b397beadc5c64348c5ccd1443ce988f9",
+    "url": "/bake/static/media/image.b397bead.png"
+  },
+  {
+    "revision": "692526e8c8dfc19e4ee545cc6b4d8681",
+    "url": "/bake/static/media/image-2.692526e8.png"
+  },
+  {
+    "revision": "1f4ba6cd14acdbd0f39c1224ccbd20c8",
+    "url": "/bake/static/media/bakemore_logo_main.1f4ba6cd.png"
+  },
+  {
+    "revision": "3507facbfef2dfe37a3446997f02781d",
+    "url": "/bake/static/media/NewPhoto1.3507facb.png"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/bake/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/bake/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/bake/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/bake/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/bake/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/bake/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/bake/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/bake/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/bake/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/bake/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/bake/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/bake/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/bake/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/bake/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/bake/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/bake/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/bake/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "196f10c6c17fcf6872537b07ac12fc88",
+    "url": "/bake/index.html"
+  }
+];
